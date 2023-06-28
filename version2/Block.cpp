4cf7b399c9691551a8f77a28432399758486a6bb
nkucszzt <99034531+nkucszzt@users.noreply.github.com>
2023-06-28 17:09:00 +0800

    version2
    添加-s功能

---
+++ version2/Block.cpp
@@ -0,0 +1,68 @@
+#include "Block.h"
+
+int Block::values[9] = { 1,2,3,4,5,6,7,8,9 };
+
+Block::Block(int _seed) :seed(_seed)
+{
+	permutation(nums, values, 9, seed);
+}
+
+Block::Block(int _seed, int _id) :seed(_seed)
+{
+	_id %= 9;
+	int n_values[8];
+	int i = 0;
+	for (; values[i] != _id; ++i);
+	for (int j = 1; j <= 8; ++j)
+	{
+		n_values[j - 1] = values[(i + j) % 9];
+	}
+	nums[8] = _id;
+	int tmp_seed = seed;
+	permutation(nums, n_values, 8, tmp_seed);
+}
+
+Block::Block(const Block& block)
+{
+	seed = block.seed;
+	for (int i = 0; i < 9; ++i)
+	{
+		nums[i] = block.nums[i];
+	}
+}
+
+Block Block::operator=(const Block& block)
+{
+	seed = block.seed;
+	for (int i = 0; i < 9; ++i)
+	{
+		nums[i] = block.nums[i];
+	}
+	return Block(block);
+}
+
+void Block::swapColumns(int i, int j)
+{
+	i = i % 3;
+	j = j % 3;
+	int tmp;
+	for (int t = 0; t < 3; ++t)
+	{
+		tmp = nums[3 * t + i];
+		nums[3 * t + i] = nums[3 * t + j];
+		nums[3 * t + j] = tmp;
+	}
+}
+
+void Block::swapRows(int i, int j)
+{
+	i = i % 3;
+	j = j % 3;
+	int tmp;
+	for (int t = 0; t < 3; ++t)
+	{
+		tmp = nums[3 * i + t];
+		nums[3 * i + t] = nums[3 * j + t];
+		nums[3 * j + t] = tmp;
+	}
+}
---
+++ version2/SudokuSolver.h
@@ -0,0 +1,17 @@
+#pragma once
+#include<string>
+
+class SudokuSolver
+{
+public:
+	SudokuSolver(int sudoku[]);
+	int solve(std::string method = "backtrack");
+	void print();
+	void toFile(FILE* f);
+	static void appendLine(FILE* f);
+private:
+	int _backtrackSolve(int holder);
+	bool _isLegal(int pos);
+	static int _find_next_empty(int* map, int size);
+	int sudoku[81];
+};
